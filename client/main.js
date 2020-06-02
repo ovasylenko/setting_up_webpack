7cf01e7d8177639cc27a868e1db226f43825cb59
Oleksii <lxvsnk@gmail.com>
2020-06-03 00:06:37 +0800

    add home

---
+++ client/main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Home = () => {
+  return <div>123</div>
+}
+
+const target = document.getElementById('root')
+
+ReactDOM.render(<Home />, target);
