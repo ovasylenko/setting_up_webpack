dca3a6519f67088f67c021e90068d771919d1182
Oleksii <lxvsnk@gmail.com>
2020-06-03 01:24:49 +0800

    fixed eslint

--- client/main.jsx
+++ client/main.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './main.scss'
 
-const Home = () => {
-  return <div>12345678</div>
+const Home = () => <div>12345678</div>
 
-}
 
 const target = document.getElementById('root')
 
-ReactDOM.render(<Home />, target);
+ReactDOM.render(<Home />, target)
